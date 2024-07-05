4ab847f0c1320ee30d30fba2b185d835e8f137f9
RicardoNoyaCamacho <83376211+RicardoNoyaCamacho@users.noreply.github.com>
2024-07-04 21:46:42 -0600

    antes de insertar por lote

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
+
+  constructor() { }
+
+  async executeSeed() {
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=9');
+
+    data.results.forEach(({name, url}) => {
+      const segments = url.split('/');
+
+      const no = +segments[segments.length - 2];
+
+      console.log({name, no});
+    })
+
+    return data.results;
+  }
+}
